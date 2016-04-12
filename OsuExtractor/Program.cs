404355171d9f3e01dea5a35b91ab8d966928170a
Dreanor <mikeundead@googlemail.com>
2016-04-13 00:09:19 +0200

    implemented stuff

---
+++ OsuExtractor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace OsuExtractor
+{
+    public class Program
+    {
+        private const string OsuFilter = "*.osz";
+        private const string MP3 = ".mp3";
+        private const string OutputDir = "output";
+
+        public static void Main(string[] args)
+        {
+            Directory.CreateDirectory(OutputDir);
+            foreach (string path in Directory.GetFiles(Environment.CurrentDirectory, OsuFilter, SearchOption.AllDirectories))
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(path))
+                {
+                    foreach (var entry in archive.Entries.Where(x => x.FullName.EndsWith(MP3)))
+                    {
+                        entry.ExtractToFile(string.Format(@"{0}\{1}", OutputDir, entry.Name, true));
+                    }
+                } 
+            }
+        }
+    }
+}
